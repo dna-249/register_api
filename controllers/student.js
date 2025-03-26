4ee5349ee02e49e6e89d409f660f878e70cae7d0
nuraalhaji <danamonuraa@gmail.com>
2025-03-26 18:19:22 +0100

    commit

--- controllers/student.js
+++ controllers/student.js
@@ -86,7 +86,7 @@ const putOneStudent =  async(req,res)=>{
         const {select3} = req.body
         const student = await Student.findByIdAndUpdate({_id},{
             $set:{
-                attend:{
+               [`attend.${1}`]:{
                     tue:select3
                 }
             }
