af607e4cf00679a75e7c946eb41c7c7271b42128
nuraalhaji <danamonuraa@gmail.com>
2025-03-29 22:09:07 +0100

    commit

--- controllers/student.js
+++ controllers/student.js
@@ -138,7 +138,7 @@ const putSetStudent = async (req,res) => {
     const {key} = req.params;
     const {value} = req.body;
     const student =  await Student.findOneAndUpdate({_id},
-        {$addToSet:
+        {$set:
           {
            [`attend.${index}`]:{
                 [`${key}`]:value
